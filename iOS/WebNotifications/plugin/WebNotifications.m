9d6ac8fd4487894cb8314318305702ff886822d0
Michal Mocny <mmocny@gmail.com>
2012-10-26 15:52:21 -0400

    .

--- iOS/WebNotifications/plugin/WebNotifications.m
+++ iOS/WebNotifications/plugin/WebNotifications.m
@@ -17,11 +17,16 @@
  under the License.
  */
 
+#define Log(fmt, ...) NSLog((@"%d: %s " fmt), __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+
 #import "WebNotifications.h"
 
 @implementation WebNotifications
 
-- (void)addNotification:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
+- (void)addNotification:(CDVInvokedUrlCommand*)command
+{
+    NSDictionary* options = [command.arguments objectAtIndex:0];
+
     // w3c options:
 	NSString *title = [options objectForKey:@"title"];
 	NSString *body = [options objectForKey:@"body"];
@@ -29,42 +34,56 @@
     //NSString *iconUrl = [options objectForKey:@"iconUrl"]; // Not supported
     
     // cordova option extensions:
-    NSUnsignedInteger delay = [[options objectForKey:@"delay"] unsignedIntegerValue];
+    NSUInteger delay = [[options objectForKey:@"delay"] unsignedIntegerValue];
     NSString *soundUrl = [options objectForKey:@"soundUrl"];
     NSInteger badgeNumber = [[options objectForKey:@"badgeNumber"] intValue];
-
+    
+    Log(@"addNotification title: %@  body: %@  tag: %@  delay: %u  badge: %u", title, body, tag, delay, badgeNumber);
+    
     //NSString *action = [options objectForKey:@"action"];
     //bool hasAction = ([[options objectForKey:@"hasAction"] intValue] == 1) ? YES : NO;
     //alertAction
-
+    
 	UILocalNotification *notif = [[UILocalNotification alloc] init];
 	notif.alertBody = [NSString stringWithFormat:@"[%@] %@: %@", tag, title, body];
     notif.timeZone = [NSTimeZone defaultTimeZone];
-
-    notif.soundName = soundName;
-    notif.applicationIconBadgeNumber = badge;
+    
+    notif.soundName = soundUrl;
+    notif.applicationIconBadgeNumber = badgeNumber;
 	
 	NSDictionary *userDict = [NSDictionary dictionaryWithObjectsAndKeys:title,@"title",body,@"body",tag,@"tag",nil];
     notif.userInfo = userDict;
 	
     if (delay != 0) {
+        Log(@"scheduleLocalNotification");
         notif.fireDate = [[NSDate date] addTimeInterval:delay];
         //notif.repeatInterval = [[repeatDict objectForKey: repeat] intValue];
         
         [[UIApplication sharedApplication] scheduleLocalNotification:notif];
     } else {
+        Log(@"presentLocalNotificationNow");
         [[UIApplication sharedApplication] presentLocalNotificationNow:notif];
     }
 }
 
-- (void)cancelNotification:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
+- (void)cancelNotification:(CDVInvokedUrlCommand*)command
+{
+//    command.callbackId;
+    NSDictionary* options = [command.arguments objectAtIndex:0];
+
     NSString *tag = [options objectForKey:@"tag"];
+    
+    Log(@"attempting cancelNotification tag: %@", tag);
+
     NSArray *notifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
     for (UILocalNotification *notification in notifications) {
         if ([[notification.userInfo objectForKey:@"tag"] isEqualToString:tag]) {
+            Log(@"Found it, cancelling tag: %@", tag);
             [[UIApplication sharedApplication] cancelLocalNotification:notification];
+            [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:0] callbackId:command.callbackId];
         }
     }
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsInt:0] callbackId:command.callbackId];
 }
 
 - (void)cancelAllNotifications:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
